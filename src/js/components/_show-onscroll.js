861dcdab461662549af1bcac86778754e2e5011b
whats0n <mrdvv@mail.ru>
2017-08-25 22:54:12 +0300

    added amazing animation

---
+++ src/js/components/_show-onscroll.js
@@ -0,0 +1,23 @@
+import { $WIN, ACTIVE } from '../_constants';
+
+export default (function() {
+
+  const $items = $('[data-show-onscroll]');
+
+  $items.each(function() {
+
+  	const $item = $(this);
+    const threshold = $item.data('show-onscroll');
+
+  	const show = () => {
+  	  if ($item.offset().top + $item.outerHeight()/100 * threshold <= $WIN.scrollTop() + $WIN.outerHeight()) {
+  	    $WIN.off('scroll', show);
+  	    $item.addClass(ACTIVE);
+  	  }
+  	};
+
+    $WIN.on('scroll', show);
+
+  });
+
+})();
