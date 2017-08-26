3ebc61899ebf9eaf0e44843f22e268c1511ecdaa
Валерий <kaktuz34@gmail.com>
2017-08-26 19:56:36 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,3 @@
-import jQuery from 'jquery';
-
-window.$ = jQuery;
-window.jQuery = jQuery;
-
-
 import common from './_common';
 import showOnScroll from './components/_show-onscroll';
 import parallax from './components/_parallax';
