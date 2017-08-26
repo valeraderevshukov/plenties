0d3ef2832c614685394bd97de049875fee49f0ca
whats0n <mrdvv@mail.ru>
2017-08-26 19:14:55 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,4 @@ import showOnScroll from './components/_show-onscroll';
 import parallax from './components/_parallax';
 import './components/_menu';
 import './components/_proggresSlider';
+import './components/_featuresActive';
